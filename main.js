6ae518667095d08e41f941d8bc8aba1cbfbdbfb8
Nikhil <nikhil.sengupta10@gmail.com>
2020-07-03 18:10:26 +0800

    Byte Club added, formatting of blog post impemented, futher optimization changes

--- main.js
+++ main.js
@@ -10,4 +10,24 @@ $(document).ready(function() {
 	$("#myResumeButton").click(function() {
 		$("#resumePanel").slideToggle("slow");
 	});
+
+	$("#byteClubButton").click(function() {
+		$("#bytePanel").slideToggle("slow");
+	});
+
+	$("#aboutmenav").click(function () {
+		$("#mePanel").slideDown("slow");
+	});
+
+	$("#hobbiesnav").click(function () {
+		$("#hobbiesPanel").slideDown("slow");
+	});
+
+	$("#resumenav").click(function () {
+		$("#resumePanel").slideDown("slow");
+	});
+
+	$("#byteclubnav").click(function () {
+		$("#bytePanel").slideDown("slow");
+	});
 });
